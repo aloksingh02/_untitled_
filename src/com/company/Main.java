8a8edea2b502829bde0d6d7d7958e07f9cdbe852
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-05-02 22:33:49 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("taking input from the user");
+        Scanner sc = new Scanner(System.in);
+
+        Boolean B1 = sc.hasNextInt();
+        System.out.println(B1);
+
+    }
+}
